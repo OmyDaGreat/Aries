1543ec1ce61776f954c9e97dd3c3b3d275df0b7e
Om Gupta <ogupta4242@gmail.com>
2024-06-11 18:34:02 -0700

    Fixed python script for gemini
    Working as aforementioned

--- src/test/java/jythonPack/aiTest.py
+++ src/test/java/jythonPack/aiTest.py
@@ -1,20 +1,12 @@
-import pathlib
-import textwrap
-
 import google.generativeai as genai
-
-from IPython.display import display
-from IPython.display import Markdown
 
 from Secrets import get
 
-
-def to_markdown(text):
-    text = text.replace('•', '  *')
-    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
-
+PROMPT = 'How are you doing?'
+MODEL = 'gemini-pro'
+print('** GenAI text: %r model & prompt %r\n' % (MODEL, PROMPT))
 
 genai.configure(api_key=get('gemini'))
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
+model = genai.GenerativeModel(MODEL)
+response = model.generate_content(PROMPT)
+print(response.text)
