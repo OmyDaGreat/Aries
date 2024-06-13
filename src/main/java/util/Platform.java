22558bd590810bc2a6632c79de0084af3b470986
Om Gupta <ogupta4242@gmail.com>
2024-06-13 15:34:35 -0700

    Fixed Build
    Removed selenium's stuff

--- src/main/java/util/Platform.java
+++ src/main/java/util/Platform.java
@@ -26,10 +26,3 @@ public enum Platform {
     return UNKNOWN;
   }
 }
-
-
-class PlatformTest {
-  public static void main(String[] args) {
-    System.out.println(System.getProperty("os.name"));
-  }
-}
