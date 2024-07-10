922fb8a55f1cbf9dfbc933be0d5bbfdbe1265bec
Rishthewizard <92958472+Rishthewizard@users.noreply.github.com>
2024-07-10 15:35:55 -0700

    mac notepad functions

--- src/main/java/util/notepad/MacNotepad.kt
+++ src/main/java/util/notepad/MacNotepad.kt
@@ -18,31 +18,78 @@ class MacNotepad: Notepad {
     }
   }
 
-  override fun writeText(text: String) {
-    //TODO: Implement writing text
+@Override
+public void writeText(String text) {
+  try {
+    // Build AppleScript command string
+    StringBuilder script = new StringBuilder("tell application \"System Events\"");
+    for (char c : text.toCharArray()) {
+      script.append("  keystroke \"").append(c).append("\"");
+    }
+    script.append("end tell");
+
+    // Execute AppleScript with keystrokes
+    ProcessBuilder builder = new ProcessBuilder("osascript", "-e", script.toString());
+    builder.start().waitFor();
+  } catch (Exception e) {
+    log.error("Error writing text to notepad", e);
+  }
+}
+
+@Override
+public void deleteText() {
+  try {
+    // Simulate multiple delete key presses (adjust count as needed)
+    for (int i = 0; i < 5; i++) {
+      ProcessBuilder builder = new ProcessBuilder("osascript", "-e", "tell application \"System Events\" key code 51 end tell");
+      builder.start().waitFor();
+      Thread.sleep(100); // Add a short delay between keystrokes
+    }
+  } catch (Exception e) {
+    log.error("Error deleting text from notepad", e);
+  }
+}
+
+@Override
+public void addNewLine() {
+  try {
+    ProcessBuilder builder = new ProcessBuilder("osascript", "-e", "tell application \"System Events\" keystroke \"return\" end tell");
+    builder.start().waitFor();
+  } catch (Exception e) {
+    log.error("Error adding new line to notepad", e);
+  }
+}
+
+@Override
+public void openNewNote() {
+  try {
+    // Simulate opening the "File" menu and selecting "New Note"
+    ProcessBuilder builder = new ProcessBuilder("osascript", "-e",
+        "tell application \"System Events\"\n" +
+        "  click menu item \"New Note\" of menu 1 of menu bar item \"File\" of menu bar 1 of application \"Notes\"\n" +
+        "end tell");
+    builder.start().waitFor();
+  } catch (Exception e) {
+    log.error("Error opening new note in notepad", e);
+  }
+}
+
+  override fun deleteNote() {
+
   }
 
-  override fun deleteText() {
-    //TODO: Implement deleting text
-  }
+override fun openExistingNote() {
 
-  override fun addNewLine() {
-    //TODO: Implement adding a new line
-  }
+}
 
-  override fun saveFileAs(name: String) {
-    //TODO: Implement saving file
+@Override
+public void closeNotepad() {
+  try {
+    ProcessBuilder builder = new ProcessBuilder("osascript", "-e",
+        "tell application \"Notes\" to quit");
+    builder.start().waitFor();
+  } catch (Exception e) {
+    log.error("Error closing notepad", e);
   }
-
-  override fun openNewFile() {
-    //TODO: Implement opening a new file
-  }
-
-  override fun closeFile() {
-    //TODO: Implement closing a file
-  }
-
-  override fun closeNotepad() {
-    //TODO: Implement closing notepad
-  }
+}
 }
