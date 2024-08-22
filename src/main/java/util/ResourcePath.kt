1a007e24cd1011c03978176071d9f400fb795c95
Rishthewizard <92958472+Rishthewizard@users.noreply.github.com>
2024-08-21 22:41:48 -0700

    temporary
    changed resourcepath.kt, changed location of aries so i need to commit :8ball: :no_mouth:

--- src/main/java/util/ResourcePath.kt
+++ src/main/java/util/ResourcePath.kt
@@ -2,18 +2,18 @@ package util
 
 import java.io.File.separator
 
-/**
- * Utility object for handling resource paths.
- */
+/** Utility object for handling resource paths. */
 object ResourcePath {
 
-    /**
-     * Returns the local resource path for the given relative path.
-     *
-     * @param path The relative path to the resource.
-     * @return The absolute path to the resource in the user's home directory.
-     */
-    fun getLocalResourcePath(path: String): String {
-        return "${System.getProperty("user.home")}${separator}Aries$separator$path"
-    }
+  /**
+   * Returns the local resource path for the given relative path.
+   *
+   * @param path The relative path to the resource.
+   * @return The absolute path to the resource in the user's home directory.
+   */
+  fun getLocalResourcePath(path: String): String {
+    return "${
+      if (Platform.detectPlatform() == Platform.MAC) "" else System.getProperty("user.home")
+    }${separator}Aries$separator$path"
+  }
 }
