5d89cce190b2d22c550ab57465de2ef558204859
Malefic <ogupta4242@gmail.com>
2024-08-23 22:40:17 -0700

    Working on Compose Check main for comments

---
+++ src/main/kotlin/aries/visual/ComposeGUI.kt
@@ -0,0 +1,169 @@
+package aries.visual
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.github.jonelo.tts.engines.VoicePreferences
+import aries.audio.LiveMic.Companion.maxWords
+import util.ResourcePath.getLocalResourcePath
+import util.audio.NativeTTS
+import util.audio.NativeTTS.Companion.loadVoicePreferences
+import util.extension.Spinner
+import util.extension.toRichHtmlString
+import java.io.File
+import java.io.FileWriter
+import java.util.*
+
+object SharedState {
+  var selectedLanguage: String = ""
+  var selectedCountry: String = ""
+}
+
+@Composable
+@Preview
+fun ComposeGUI() {
+  writeVoicePreferencesToFile(getLocalResourcePath("voicePreferences.txt"))
+  loadVoicePreferences()
+
+  var selectedLanguage by remember { mutableStateOf("") }
+  var selectedCountry by remember { mutableStateOf("") }
+  var selectedGender by remember { mutableStateOf("") }
+
+  updateGUIFromPreferences(
+    setSelectedLanguage = { selectedLanguage = it },
+    setSelectedCountry = { selectedCountry = it },
+    setSelectedGender = { selectedGender = it }
+  )
+
+  val languages = Locale.getISOLanguages().toList()
+  val countries = Locale.getISOCountries().toList()
+  val genders = listOf("MALE", "FEMALE")
+
+  selectedLanguage = languages[0]
+  selectedCountry = countries[0]
+  selectedGender = genders[0]
+
+  // Update SharedState with the selected language and country
+  SharedState.selectedLanguage = selectedLanguage
+  SharedState.selectedCountry = selectedCountry
+
+  Column(modifier = Modifier.padding(16.dp)) {
+    DropdownMenu(expanded = true, onDismissRequest = {}) {
+      languages.forEach { language ->
+        DropdownMenuItem(onClick = {
+          selectedLanguage = language
+          SharedState.selectedLanguage = language // Update SharedState
+        }) { Text(language) }
+      }
+    }
+    DropdownMenu(expanded = true, onDismissRequest = {}) {
+      countries.forEach { country ->
+        DropdownMenuItem(onClick = {
+          selectedCountry = country
+          SharedState.selectedCountry = country // Update SharedState
+        }) { Text(country) }
+      }
+    }
+    DropdownMenu(expanded = true, onDismissRequest = {}) {
+      genders.forEach { gender ->
+        DropdownMenuItem(onClick = { selectedGender = gender }) { Text(gender) }
+      }
+    }
+
+    Text(commandInfo.toRichHtmlString())
+
+    Button(
+      onClick = {
+        NativeTTS.voiceLanguage(selectedLanguage)
+        NativeTTS.voiceCountry(selectedCountry)
+        NativeTTS.voiceGender(VoicePreferences.Gender.valueOf(selectedGender))
+        NativeTTS.saveVoicePreferences()
+      }
+    ) {
+      Text("Apply Settings")
+    }
+
+    Spinner(
+      value = maxWords,
+      onValueChange = { newValue -> maxWords = newValue },
+      range = 1..100000,
+    )
+  }
+}
+
+private fun writeVoicePreferencesToFile(filePath: String) {
+  val content =
+    """
+        language=en
+        country=US
+        gender=MALE
+        maxWords=40
+      """
+      .trimIndent()
+  File(filePath).apply {
+    if (!exists()) {
+      createNewFile()
+      FileWriter(this).use { it.write(content) }
+    }
+  }
+}
+
+private fun updateGUIFromPreferences(
+  setSelectedLanguage: (String) -> Unit,
+  setSelectedCountry: (String) -> Unit,
+  setSelectedGender: (String) -> Unit
+) {
+  val preferences = NativeTTS.voicePreferences
+  setSelectedLanguage(preferences.language)
+  setSelectedCountry(preferences.country)
+  setSelectedGender(preferences.gender.name)
+}
+
+private const val commandInfo =
+  """
+<strong>Hey Aries...</strong><br/>
+- "write special [text]": Writes special characters.<br/>
+- "write [text]": Writes the specified text.<br/>
+- "search [query]": Searches Google for the specified query.<br/>
+- "ask gemini [query]": Queries gemini and processes the response.<br/>
+- "[query]": Queries gemini and processes the response.<br/>
+<strong>Keyboard Commands:</strong><br/>
+- "control shift [text]": Types text with Control + Shift modifier.<br/>
+- "shift [text]": Types text with Shift modifier.<br/>
+- "control [text]": Types text with Control modifier.<br/>
+- "command [text]": Types text with Command modifier (on macOS).<br/>
+- "arrow [direction(s)]": Moves the arrow keys in the specified direction(s).<br/>
+- "cap": Presses the Caps Lock key.<br/>
+- "switch window [number]": Switches to the specified window.<br/>
+- "f [number]": Presses the specified function key.<br/>
+- "alt f [number]": Presses ALT + specified function key.<br/>
+- "windows shift [text]": Types text with Windows + Shift modifier.<br/>
+- "windows [text]": Types text with Windows modifier.<br/>
+- "command shift [text]": Types text with Command + Shift modifier.<br/>
+- "enter": Presses the enter key.<br/>
+- "tab": Presses the Tab key.<br/>
+<strong>Mouse Commands:</strong><br/>
+- "left click": Performs a left mouse click.<br/>
+- "right click": Performs a right mouse click.<br/>
+- "left press": Presses the left mouse button.<br/>
+- "left release": Releases the left mouse button.<br/>
+- "right press": Presses the right mouse button.<br/>
+- "right release": Releases the right mouse button.<br/>
+- "middle click": Performs a middle mouse click.<br/>
+- "mouse [direction(s)]": Moves the mouse in the specified direction(s).<br/>
+- "scroll [direction(s)]": Scrolls the mouse wheel in the specified direction(s).<br/>
+<strong>Notepad-specific commands:</strong><br/>
+- "open notepad": Opens the notepad.<br/>
+- "close notepad": Closes the notepad.<br/>
+- "open new": Opens a new file in notepad.<br/>
+- "delete everything": Deletes all text in notepad.<br/>
+- "save file [name]": Saves the file with the specified name.<br/>
+- "save file as [name]": Saves the file with the specified name (alternative phrasing).
+"""
---
+++ src/main/kotlin/util/audio/PorcupineUtils.kt
@@ -0,0 +1,76 @@
+package util.audio
+
+import ai.picovoice.leopard.Leopard
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.time.Duration
+import java.time.Instant
+import javax.sound.sampled.*
+import kotlin.math.abs
+import aries.audio.LiveMic.Companion.leopardthing
+import util.extension.trueContains
+
+fun processAudio(keywordDetected: () -> Unit, onSilence: () -> Unit, isRecording: () -> Boolean) {
+  val info = DataLine.Info(TargetDataLine::class.java, null)
+  if (!AudioSystem.isLineSupported(info)) {
+    throw LineUnavailableException("No line matching provided info found.")
+  }
+  val leopard = leopardthing.build()
+  val line = AudioSystem.getLine(info) as TargetDataLine
+  val format = AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 16000F, 16, 1, 2, 16000F, false)
+  line.open(format)
+  line.start()
+  val buffer = ShortArray(33332) // 20,000 is too little; 40,000 is too much: slow to process
+  val byteBuffer = ByteArray(buffer.size * 2)
+  var silenceFrames: Instant? = null
+  val amplitudeThreshold = 1000
+
+  while (true) {
+    if (isRecording()) {
+      if (isSilence(buffer, amplitudeThreshold)) {
+        if (Duration.between(silenceFrames ?: Instant.now(), Instant.now()).toSeconds() >= 7) {
+          onSilence()
+          line.open(format)
+          line.start()
+        }
+      } else {
+        silenceFrames = Instant.now()
+      }
+    } else {
+      val bytesRead = line.read(byteBuffer, 0, byteBuffer.size)
+      if (bytesRead <= 0) continue
+      ByteBuffer.wrap(byteBuffer).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer()[buffer]
+
+      val conversion = convertAudioToText(buffer, leopard)
+
+      if (conversion.trueContains("Hey Aries", "Aries", "Harry")) {
+        line.close()
+        keywordDetected()
+        silenceFrames = Instant.now()
+      }
+    }
+  }
+}
+
+/**
+ * Determines if a given audio buffer contains silence. Silence is defined as having an average
+ * amplitude below a specified threshold.
+ *
+ * @param buffer The audio buffer to analyze.
+ * @param threshold The amplitude threshold below which the audio is considered silent.
+ * @return True if the buffer is considered silent, false otherwise.
+ */
+fun isSilence(buffer: ShortArray, threshold: Int): Boolean {
+  var sum = 0.0
+  for (short in buffer) sum += abs(short.toInt())
+  val average = sum / buffer.size
+  return average < threshold
+}
+
+fun convertAudioToText(buffer: ShortArray?, leopard: Leopard): String {
+  return try {
+    leopard.process(buffer).transcriptString
+  } catch (e: Exception) {
+    ""
+  }
+}
