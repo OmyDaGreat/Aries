5570742f31517e32728e0464818d398238746d91
Rishthewizard <92958472+Rishthewizard@users.noreply.github.com>
2024-09-07 14:06:20 -0700

    Update FileExtension.kt

--- src/main/kotlin/util/extension/FileExtension.kt
+++ src/main/kotlin/util/extension/FileExtension.kt
@@ -23,9 +23,9 @@ const val openNotepad =
 const val leolibwin =
   "https://www.dropbox.com/scl/fi/mztcz5q5p8qiv91vfchi8/libpv_leopard_jni.dll?rlkey=9aumenejyt39iewm2d8kwze6b&st=qaw0fewr&dl=1"
 
-// URL of leopard library for mac (86 64 rn)
+// URL of leopard library for mac (leopard rn)
 const val leolibmac =
-  "https://www.dropbox.com/scl/fi/6j3nw3vj1j404zr2tr2jn/libpv_leopard_jni.dylib?rlkey=cpqt4zg7bnnkzjlwrp2gl8ptn&st=t1hvla20&dl=1"
+  "https://www.dropbox.com/scl/fi/6j3nw3vj1j404zr2tr2jn/libpv_leopard_jni.dylib?rlkey=cpqt4zg7bnnkzjlwrp2gl8ptn&st=76x93y4h&dl=1"
 
 // URL of leopard library for linux
 const val leoliblin =
